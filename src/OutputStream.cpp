c6986c69eda8ff2f2dfb6e393177d9da390fdf9e
Tom Briggs <tb1516@ic.ac.uk>
2024-03-11 07:45:54 +0000

    fixed early exit in OutputStream

--- src/OutputStream.cpp
+++ src/OutputStream.cpp
@@ -19,8 +19,11 @@ OutputStream::OutputStream(const char* destination_url, AVDictionary* output_opt
         avformat_alloc_output_context2(&m_output_format_context, NULL, "mpeg", m_destination_url);
     }
     if (!m_output_format_context)
+    {
         cleanup();
         exit(1);
+    }
+        
 
     /*assign things to the output format from the output format context*/
     m_output_format = m_output_format_context->oformat;
@@ -57,7 +60,8 @@ OutputStream::OutputStream(const char* destination_url, AVDictionary* output_opt
         m_audio_stream->id = m_output_format_context->nb_streams-1;
         AVCodecContext* c;
         c = avcodec_alloc_context3(m_output_codec);
-        if (!c) {
+        if (!c) 
+        {
             fprintf(stderr, "Could not alloc an encoding context\n");
             cleanup();
             exit(1);
@@ -70,9 +74,11 @@ OutputStream::OutputStream(const char* destination_url, AVDictionary* output_opt
             (m_output_codec)->sample_fmts[0] : AV_SAMPLE_FMT_FLTP;
         m_output_codec_context->bit_rate    = bit_rate;
         m_output_codec_context->sample_rate = sample_rate;
-        if ((m_output_codec)->supported_samplerates) {
+        if ((m_output_codec)->supported_samplerates) 
+        {
             m_output_codec_context->sample_rate = (m_output_codec)->supported_samplerates[0];
-            for (i = 0; (m_output_codec)->supported_samplerates[i]; i++) {
+            for (i = 0; (m_output_codec)->supported_samplerates[i]; i++) 
+            {
                 if ((m_output_codec)->supported_samplerates[i] == sample_rate)
                     m_output_codec_context->sample_rate = sample_rate;
             }
@@ -83,7 +89,10 @@ OutputStream::OutputStream(const char* destination_url, AVDictionary* output_opt
  
         /* Some formats want stream headers to be separate. */
         if (m_output_format_context->oformat->flags & AVFMT_GLOBALHEADER)
+        {
             c->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+        }
+           
 
     }
 
@@ -100,7 +109,8 @@ OutputStream::OutputStream(const char* destination_url, AVDictionary* output_opt
     ret = avcodec_open2(m_output_codec_context, m_output_codec, &opt);
     /* delete the temp dictionary*/
     av_dict_free(&opt);
-    if (ret < 0) {
+    if (ret < 0) 
+    {
         fprintf(stderr, "Could not open audio codec\n");
         cleanup();
         exit(1);
@@ -142,6 +152,26 @@ OutputStream::OutputStream(const char* destination_url, AVDictionary* output_opt
     }
 
     av_dump_format(m_output_format_context, 0, m_destination_url, 1);
+
+        /* open the output file, if needed */
+    if (!(m_output_format->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&m_output_format_context->pb, m_destination_url, AVIO_FLAG_WRITE);
+        if (ret < 0) 
+        {
+            fprintf(stderr, "Could not open '%s'\n", m_destination_url);
+            cleanup();
+            exit(1);
+        }
+    }
+
+        /* Write the stream header, if any. */
+    ret = avformat_write_header(m_output_format_context, &m_output_options);
+    if (ret < 0) 
+    {
+        fprintf(stderr, "Error occurred when opening output file\n");
+        cleanup();
+        exit(1);
+    }
     
 }
 /*normal destructor*/
