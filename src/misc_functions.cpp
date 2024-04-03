4edd84bf3f847a36b15a55db5743476152eb7908
chees-eater-tb1516 <161821798+chees-eater-tb1516@users.noreply.github.com>
2024-04-03 10:17:18 +0100

    Update misc_functions.cpp

--- src/misc_functions.cpp
+++ src/misc_functions.cpp
@@ -21,3 +21,12 @@ struct timespec get_timespec_from_ticks(int ticks)
 
     return output;
 }
+
+/*calculates sleep time required and sleeps the thread accordingly, put this function at the end of the loop:
+* assumes the iteration start time is equal to the end time of the previous iteration*/
+void fondue_sleep(std::chrono::_V2::steady_clock::time_point &end_time, const std::chrono::duration<double> &loop_duration)
+{
+    std::chrono::duration<double> sleep_time = loop_duration - (std::chrono::steady_clock::now() - end_time);
+    std::this_thread::sleep_for(sleep_time);
+    end_time = std::chrono::steady_clock::now();
+}
