4fe9ec63ee17c04e0b606d99881e641f842b46b4
chees-eater-tb1516 <tb1516@ic.ac.uk>
2024-04-09 17:19:04 +0100

    isolated issue to within 'no input' constructor

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ int main ()
     OutputStream sink("test.mp3", output_options, DEFAULT_SAMPLE_RATE, DEFAULT_BIT_RATE);
     InputStream test_input("/home/tb1516/cppdev/fondue/audio_sources/main_theme.mp3", NULL, sink.get_output_codec_context(), input_options, 
                             SourceTimingModes::freetime, DefaultSourceModes::white_noise);
-    try
+    /*try
     {
         test_input = InputStream ("-f alsa -i hw:1,0 -ar 44100 -ac 2", sink, 
                             SourceTimingModes::realtime, DefaultSourceModes::white_noise);
@@ -150,9 +150,9 @@ int main ()
     catch (const char* exception)
     {
         std::cout<<exception<<": failed to correctly access input, using default source\n";
-        /*implement 'no input' constructor and call it here*/
+    
         test_input = InputStream(sink, DefaultSourceModes::white_noise);
-    }
+    }*/
     
     
     
